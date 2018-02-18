5ceed342b63a981847b32b0a45595d03a46c220a
Nico Castro <nicollecastrog@gmail.com>
2018-02-18 19:05:06 +0000

    refactoring

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,61 +5,47 @@ import IndvTasks from "../Tasks/IndvTasks";
 
 class List extends Component {
     constructor(props, context) {
-        super(props, context);
-        this.state = {
-            tasks: [],
-            newTask: "",
-            
+      super(props, context);
+      this.state = {
+        tasks: []
+      };
+    }
 
+    addTask = (formSubmitEvent) => {
+      formSubmitEvent.preventDefault(); // blocking form's default behaviour of refreshing the page upon submiting the form
+      let newTaskArr = [...this.state.tasks]; // using array spread syntax to create a new array where all added tasks will be added
+
+      if (this._userInput.value.trim() !== "") { // conditional to check whether input field has string; use trim to remove whitespace at beginning and end
+        const newTask = {
+          title: this._userInput.value,
+          key: Date.now,
+          done: false
         };
+
+        newTaskArr.unshift(newTask); // adding a new task (an object made up of three keys) to the end of the tasks array
+
+        this.setState({
+          tasks: newTaskArr
+        });
+
+        this._userInput.value = ""; // clears the add task input
+        console.log(`added ${newTask.title} to list`);
+      }
     }
-
-    // taskComplete = (done) => {
-    //     console.log(done)
-    //     this.state.tasks[done].done = !this.state.tasks[done].done
-
-    //     this.setState({
-    //         tasks: this.state.tasks
-    //     })
-    // }
-
-    addTask = (newTask) => {
-        let newTaskArr = this.state.tasks //creating a new array where all added tasks will be added
-        if (this._userInput.value !== "") { //conditional to check whether input field has string
-            newTaskArr.unshift({
-                title: this._userInput.value,  // adding an object with three keys to the new array.
-                key: Date.now,
-                //done: false
-            })
-            this.setState({
-                tasks: newTaskArr
-            })
-            this._userInput.value = "";
-        }
-        console.log(newTaskArr);
-        newTask.preventDefault(); //blocking form's default behaviour of clear out all input at refresh.
-    }
-
 
     render() {
-        return (
-            <div className="mainList">
-                <div className="header">
-                    <form onSubmit={this.addTask}>
-                        <input ref={(a) => this._userInput = a} placeholder="enter task">
-                        </input>
-                        <button type="submit"> Add Task </button>
-                    </form>
-                </div>
-                <IndvTasks tasks={this.state.tasks}/>
-            </div>
-        );
+      return (
+        <div className="mainList">
+          <div className="header">
+            <form onSubmit={this.addTask}>
+              <input ref={(a) => this._userInput = a} placeholder="enter task" />
+              <button type="submit">Add Task</button>
+            </form>
+          </div>
+          <IndvTasks tasks={this.state.tasks}/>
+        </div>
+      );
     }
 }
 
 export default List;
-
-
-// onClick = {() => {
-//     this.addTask(this.state.newTask)
-// }}> <span className="lead">
--- src/components/Tasks/IndvTasks.js
+++ src/components/Tasks/IndvTasks.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 
 class IndvTasks extends Component {
-
-    printTasks = (todo) => {
-        return <div>
-            <input type="checkbox" onChange={() => { this.props.taskComplete(this.props.index) }}/>
-            <label key={todo.key}>{todo.title}>{todo.done} </label>
-            </div>
+    printTask = (todo) => {
+      return (
+        <div key={todo.key}>
+          <input type="checkbox" onChange={() => { console.log("toggle task") }}/>
+          <label key={todo.key}>{todo.title}</label>
+        </div>
+      );
     }
 
     render() {
-        let tasksArr = this.props.tasks;
-        let listItems = tasksArr.map(this.printTasks);
-
-        return (
-           <div>
-                {listItems} 
-           </div>
-        );
+      return (
+        <div>
+          {this.props.tasks.map(this.printTask)}
+        </div>
+      );
     }
 };
 
